20f8f4376a199f6c177757191a5960b50c54ae6f
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-08-08 23:03:42 -0300

    Criado base pagina de projetos, com botão para novo projeto e preparado para receber projetos do banco

--- src/componentes/paginas/Projetos/Projetos.js
+++ src/componentes/paginas/Projetos/Projetos.js
@@ -1,7 +1,9 @@
 import { useLocation } from 'react-router-dom';
 
 import Mensagem from '../../layout/Mensagem/Mensagem';
+import Container from '../../layout/Container/Container';
 import styles from './Projetos.module.css';
+import LinkButton from '../../layout/LinkButton/LinkButton';
 
 function Projetos() {
   const location = useLocation();
@@ -12,11 +14,17 @@ function Projetos() {
   }
   
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/novoProjeto" text="Criar Projeto"/>
+      </div>
       {mensagem && 
         <Mensagem tipo="sucesso" mensagem={mensagem}/>
       }
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
