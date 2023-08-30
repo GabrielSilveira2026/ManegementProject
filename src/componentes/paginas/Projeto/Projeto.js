eedf515892f6af7fe46d73fe20b5460f0c874287
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2023-08-30 19:10:24 -0300

    Adicionado loading em Projeto, implementado botão de exibição de formulario e detalhes do projeto, e ajustes visuais

--- src/componentes/paginas/Projeto/Projeto.js
+++ src/componentes/paginas/Projeto/Projeto.js
@@ -1,13 +1,17 @@
 import styles from './Projeto.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../../layout/Loading/Loading';
+import Container from '../../layout/Container/Container';
 
 function Projeto() {
     const { id } = useParams()
     const [projeto, setProjeto] = useState([])
+    const [mostraProjetoForm, setMostraProjetoForm] = useState(false)
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projetos/${id}`, {
+        setTimeout(()=>{
+            fetch(`http://localhost:5000/projetos/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,12 +22,56 @@ function Projeto() {
                 setProjeto(dados)
             })
             .catch((erro) => { console.log(erro); })
+        },200)
     }, [])
 
+    function toggleProjectForm(){
+        setMostraProjetoForm(!mostraProjetoForm)
+    }
+
     return (
-        <div>
-            {projeto?.nome}
-        </div>
+        <>
+            {
+                projeto.nome?
+                    (
+                        <div className={styles.projetoDetalhes}>
+                            <Container customClass="column">
+                                <div className={styles.containerDetalhes}>
+                                    <h1>Projeto: {projeto.nome}</h1>
+                                    <button className={styles.btn} onClick={toggleProjectForm}>
+                                        {
+                                            !mostraProjetoForm?
+                                                "Editar Projeto"
+                                            :
+                                                "Fechar"
+                                        }
+                                    </button>
+                                    {
+                                        !mostraProjetoForm?
+                                            <div className={styles.projetoInfo}>
+                                                <p>
+                                                    <span>Categoria: </span> {projeto?.categoria?.nome}
+                                                </p>
+                                                <p>
+                                                    <span>Total de orçamento</span> R${projeto.orcamento}
+                                                </p> 
+                                                <p>
+                                                    <span>Total utilizado</span> R${projeto.custos}
+                                                </p> 
+                                            </div>
+                                        :
+                                            <div className={styles.projetoInfo}>
+                                                <p>Formulario</p>
+                                            </div>
+                                    }
+                                </div>
+                            </Container>
+                        </div>
+                    )
+                :
+                    <Loading/>
+            }
+        </>
     )
 }
 
